4919d0076f409f4e646652e64f15e2461c3d4e11
James Dabbs <jamesdabbs@gmail.com>
2016-04-18 11:04:57 -0400

    Add ng-repeat

--- js/main.js
+++ js/main.js
@@ -20,10 +20,12 @@
 
    // AnimalsController
    function AnimalsController() {
-     var animal = animals[0];
+     this.all = [
+       { species: "Buffalo", type: "Mammal", picture: "https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg" },
+       { species: "Penguin", type: "Bird", picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg/800px-South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg" },
+       { species: "Platypus", type: "???", picture: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Wild_Platypus_4.jpg" }
+     ];
 
-     this.name = animal.species;
-     this.type = animal.type;
-     this.picture = animal.picture;
+     this.types = ["Mammal", "Bird", "???"]
    }
 })();
