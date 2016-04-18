12e47f05871a37c1e593b19f49ec573c98e9bf33
James Dabbs <jamesdabbs@gmail.com>
2016-04-18 11:25:33 -0400

    Add ng-class

--- js/main.js
+++ js/main.js
@@ -25,9 +25,9 @@
        { species: "Penguin", type: "Bird", picture: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg/800px-South_Shetland-2016-Deception_Island%E2%80%93Chinstrap_penguin_%28Pygoscelis_antarctica%29_04.jpg" },
        { species: "Platypus", type: "???", picture: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Wild_Platypus_4.jpg" },
        { species: "Ostrich", type: "Bird", picture: "" },
+       { species: "T-Rex", type: "Dinosaur", picture: "" },
        { species: "Bear", type: "Mammal", picture: "" },
        { species: "Baboon", type: "Mammal", picture: "" },
-       { species: "T-Rex", type: "Dinosaur", picture: "" },
        { species: "Velociraptor", type: "Dinosaur", picture: "" }
      ];
 
